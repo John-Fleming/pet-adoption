f6991f36aaae99feb466e5e136571207c5b425a1
John Fleming <johndeclanfleming@gmail.com>
2020-01-22 19:59:24 -0600

    wrote javascript functions to loop through array and push to DOM

--- main.js
+++ main.js
@@ -1,1 +1,88 @@
-console.log('pet adoption')
+const pets = [
+    {
+      name: "Dusty",
+      color: "Green",
+      specialSkill: "Gives sincere apologies.",
+      type: "cat",
+      imageUrl: "http://kittentoob.com/wp-content/uploads/2015/06/funny-cat-with-a-towel.jpg"
+    },
+    {
+      name: "Trouble",
+      color: "Poop-Colored",
+      specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
+      type: "dino",
+      imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg"
+    },
+    {
+      name: "Whiskers",
+      color: "Yellow",
+      specialSkill: "Can prove he is a real man by drinking whiskey.",
+      type: "dino",
+      imageUrl: "https://mydinosaurs.com/wp-content/uploads/2017/02/2-3-600x400.jpg"
+    },
+    {
+      name: "Coco",
+      color: "Black",
+      specialSkill: "Burps minimally.",
+      type: "dog",
+      imageUrl: "http://cdn.akc.org/content/article-body-image/funny-pembroke_welsh_corgi.jpg"
+    },
+    {
+      name: "Spooky",
+      color: "Brown",
+      specialSkill: "Comfortable in the outdoors for up to eight hours.",
+      type: "cat",
+      imageUrl: "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"
+    },
+    {
+      name: "Tiger",
+      color: "Black",
+      specialSkill: "Can read (but cannot understand) Hebrew.",
+      type: "dog",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Basset_Hound_600.jpg"
+    }
+  ];
+
+  // push to DOM function
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+
+  // array loop function
+
+  const petCardCreator = () => {
+      let domString = '';
+      for (pet in pets) {
+          if (pets[pet].type === 'cat') {
+            domString += `<div class="pet-card">`;
+            domString +=   `<h2 class="pet-name">${pets[pet].name}</h2>`;
+            domString +=   `<img src=${pets[pet].imageUrl} alt="picture of a ${pets[pet].type}">`;
+            domString +=   `<p class="pet-color">${pets[pet].color}</p>`;
+            domString +=   `<p class="pet-special-skill">${pets[pet].specialSkill}</p>`;
+            domString +=   `<p class="pet-type-cat">${pets[pet].type}</p>`;
+            domString += `</div>`;
+          } else if (pets[pet].type === 'dog') {
+                domString += `<div class="pet-card">`;
+                domString +=   `<h2 class="pet-name">${pets[pet].name}</h2>`;
+                domString +=   `<img src=${pets[pet].imageUrl} alt="picture of a ${pets[pet].type}">`;
+                domString +=   `<p class="pet-color">${pets[pet].color}</p>`;
+                domString +=   `<p class="pet-special-skill">${pets[pet].specialSkill}</p>`;
+                domString +=   `<p class="pet-type-dog">${pets[pet].type}</p>`;
+                domString += `</div>`;
+            } else if (pets[pet].type === 'dino') {
+                    domString += `<div class="pet-card">`;
+                    domString +=   `<h2 class="pet-name">${pets[pet].name}</h2>`;
+                    domString +=   `<img src=${pets[pet].imageUrl} alt="picture of a ${pets[pet].type}">`;
+                    domString +=   `<p class="pet-color">${pets[pet].color}</p>`;
+                    domString +=   `<p class="pet-special-skill">${pets[pet].specialSkill}</p>`;
+                    domString +=   `<p class="pet-type-dino">${pets[pet].type}</p>`;
+                    domString += `</div>`;
+                }
+      printToDom('pet-card-container', domString);
+    }
+  };
+
+  petCardCreator();
